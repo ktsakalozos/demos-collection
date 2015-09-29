41fc0f8a3c7fecd4e774702b15bde3e7614c17cb
Ubuntu <azureuser@orch-cc.orch-cc.f6.internal.cloudapp.net>
2015-09-29 16:45:01 +0000

    Running rolling topology in cluster mode

--- storm/0.9.1/RollingTopWords/run.sh
+++ storm/0.9.1/RollingTopWords/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 
-storm jar /usr/lib/storm/contrib/storm-starter/storm-starter-0.9.1.2.1.3.0-563-jar-with-dependencies.jar storm.starter.RollingTopWords
+storm jar /usr/lib/storm/contrib/storm-starter/storm-starter-0.9.1.2.1.3.0-563-jar-with-dependencies.jar storm.starter.RollingTopWords production-topology remote
